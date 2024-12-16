16c5691f0a07457404a54358ed3ad68b7ac6a8d6
iampurna <linkwithpurna@gmail.com>
2024-12-16 12:09:11 +0545

    Complete blogApp

---
+++ Migrations/20241216055510_finalmigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazingBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class finalmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FirstName", "Hash", "LastName", "Salt" },
+                values: new object[] { "linkwithpurna@gmail.com", "Purna", "yeaghakshencdsskkokwwrnds/=", "Lungeli", "uthedggdggsgs" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FirstName", "Hash", "LastName", "Salt" },
+                values: new object[] { "abhayprince@outlook.com", "Abhay", "dfhkhsdskdfffgfjgfjgkdfhjgkdf/=", "Prince", "dshfksdhfk" });
+        }
+    }
+}
